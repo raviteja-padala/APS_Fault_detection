7c84948cd29b81d694e74cf068f973e85f88dd50
raviteja <scorpio_raviteja@yahoo.co.in>
2023-01-11 21:16:47 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sensor.entity import config_entity
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.components import data_ingestion
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 
 if __name__=="__main__":
@@ -18,8 +19,13 @@ if __name__=="__main__":
           print(data_ingestion_config.to_dict())
           
           data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
-          print("Data ingestion done")
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation= DataValidation(data_validation_config=data_validation_config, data_ingestion_artifact = data_ingestion_artifact)
+
+          data_validation_artifact = data_validation.initiate_data_validation()
+          print("Data Validation Done")
 
      except Exception as e:
           print(e)
