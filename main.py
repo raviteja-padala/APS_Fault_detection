b349123b571dabc31d7768364096a8df1d10954a
raviteja <scorpio_raviteja@yahoo.co.in>
2023-01-12 23:03:18 +0530

    Model Evaluation added

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
+from sensor.components.model_evaluation import ModelEvaluation
 
 
 if __name__=="__main__":
@@ -46,6 +47,17 @@ if __name__=="__main__":
 
           model_trainer_artifact = model_trainer.initiate_model_trainer()
           print("Model Training Done")
+
+          #Model Evaluation
+          model_eval_config= config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval = ModelEvaluation(model_eval_config = model_eval_config , data_ingestion_artifact=data_ingestion_artifact , data_transformation_artifact=data_transformation_artifact , model_trainer_artifact=model_trainer_artifact )
+
+          model_eval_artifact = model_eval.initiate_model_evaluation()
+          print("Model Evaluation Done")
+
+
      except Exception as e:
           print(e)
+
+
 
